61963bc5805f0d6528775b68d563100de1b138c7
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-17 21:09:14 +0000

    create form for new tweet and html/css view for new tweet

--- server/lib/date-adjust.js
+++ server/lib/date-adjust.js
@@ -1,10 +1,8 @@
 "use strict";
-
 // Require fs to write to initial-tweets.json
 const fs = require("fs");
 // Requiring a JSON file automatically parses it and returns the data.
 let tweetsJSON = require('../data-files/initial-tweets.json');
-
 // Write recent dates to initial-tweets.json
 // Specifically sync to not interfere with student functions
 module.exports = () => {
@@ -13,17 +11,11 @@ module.exports = () => {
   const oneDayMs = 1000 * 60 * 60 * 24
   // Subtract one day in milliseconds (oneDayMs) times the tweets length minus the current index.
   // This keeps the newest tweets at the bottom, and allows for further tweets to be added.
-
-  /*tweetsJSON = tweetsJSON.map((tweet, index) => tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index))); - return an erro in console /vagrant/w4/tweeter/server/lib/date-adjust.js:16
-  tweetsJSON = tweetsJSON.map((tweet, index) => tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index)));
-  ^
-
-TypeError: Cannot create property 'created_at' on number '1605376912995'
-at tweetsJSON.map (/vagrant/w4/tweeter/server/lib/date-adjust.js:16:66)
-
-*/
-
-  tweetsJSON = tweetsJSON.map((tweet, index) => tweet = Date.now() - (oneDayMs * (tweetsJSON.length - index)));
+ 
+  tweetsJSON = tweetsJSON.map((tweet, index) => {
+    tweet.created_at = Date.now() - (oneDayMs * (tweetsJSON.length - index));
+    return tweet;
+  });
   // Re-write the tweets with the new date values.
   fs.writeFileSync('server/data-files/initial-tweets.json', JSON.stringify(tweetsJSON, null, 2), { encoding: "utf8" });
 };
